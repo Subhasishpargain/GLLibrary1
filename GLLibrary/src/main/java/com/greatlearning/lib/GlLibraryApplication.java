c64996bc11ec55f19e99a5c5f0df4e04a39d917d
Subhasishpargain <135615007+Subhasishpargain@users.noreply.github.com>
2023-09-20 20:50:04 +0530

    Add files via upload

---
+++ GLLibrary/src/main/java/com/greatlearning/lib/GlLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.greatlearning.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlLibraryApplication.class, args);
+	}
+
+}
